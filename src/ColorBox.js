9eec6a4634d05068aa854039e0e53327a9980ab3
Mohammed Elattar <mohammed.elattar@metro-markets.de>
2021-04-08 14:26:35 +0200

    Style ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,8 +5,13 @@ export class ColorBox extends Component {
     render() {
         return (
             <div style={{backgroundColor: this.props.backgroundColor}} className='ColorBox'>
-                <span>{this.props.name}</span>
-                <span>More</span>
+                <div className='copy-container'>
+                    <div className='box-content'>
+                        <span>{this.props.name}</span>
+                    </div>
+                    <button className='copy-button'>Copy</button>
+                </div>
+                <span className='see-more'>More</span>
             </div>
         )
     }
