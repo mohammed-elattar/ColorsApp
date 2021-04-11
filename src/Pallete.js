e2f9a900ed30f944a5394ec8ea270375814c6480
Mohammed Elattar <mohammed.elattar@metro-markets.de>
2021-04-11 22:11:09 +0200

    add Navbar

--- src/Pallete.js
+++ src/Pallete.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
+import Navbar from './Navbar';
 import './Pallete.css';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-
 export default class Pallete extends Component {
     constructor(props) {
         super(props);
@@ -19,13 +17,7 @@ export default class Pallete extends Component {
         const colorsBoxes = colors[level].map(color => <ColorBox backgroundColor={color.hex} name={color.name}/>);
         return (
             <div className='Pallete'>
-                <Slider 
-                    defaultValue={level}
-                    min={100}
-                    max={900}
-                    onAfterChange={this.changeLevel}
-                    step={100}
-                />
+            <Navbar {...this.state} changeLevel={this.changeLevel} />
                 <div className='Pallete-colors'>
                     {colorsBoxes}
                 </div>
